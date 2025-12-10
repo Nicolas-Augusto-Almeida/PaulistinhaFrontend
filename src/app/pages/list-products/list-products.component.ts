2c64c869294cc3afe5841d959e4f0dd1144ba94e
Nicolas Augusto <nick.nogueira02@gmail.com>
2025-12-10 19:16:27 -0300

    :lipstick: changed: styles for product and employees list

--- src/app/pages/list-products/list-products.component.ts
+++ src/app/pages/list-products/list-products.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { ProdutoServiceService, Produto } from '../../services/produto-service.service';
+import {
+  ProdutoServiceService,
+  Produto,
+} from '../../services/produto-service.service';
 
 @Component({
   selector: 'app-list-products',
   standalone: false,
   templateUrl: './list-products.component.html',
-  styleUrls: ['./list-products.component.css']
+  styleUrls: ['./list-products.component.css'],
 })
 export class ListProductsComponent implements OnInit {
-
   produtos: Produto[] = [];
+  produtosFiltrados: Produto[] = [];
 
-  constructor(private produtoService: ProdutoServiceService) { }
+  constructor(private produtoService: ProdutoServiceService) {}
 
   ngOnInit(): void {
     this.produtoService.listarProdutos().subscribe({
-      next: (data) => this.produtos = data,
-      error: (err) => console.error('Erro ao carregar produtos', err)
+      next: (data) => {
+        this.produtos = data;
+        this.produtosFiltrados = data;
+        console.log(data);
+      },
+      error: (err) => console.error('Erro ao carregar produtos', err),
     });
   }
 
@@ -27,14 +34,29 @@ export class ListProductsComponent implements OnInit {
 
     this.produtoService.excluirProduto(id).subscribe({
       next: () => {
-        this.produtos = this.produtos.filter(produto => produto.id !== id);
+        this.produtos = this.produtos.filter((produto) => produto.id !== id);
         console.log(`Produto com ID ${id} excluído com sucesso.`);
       },
       error: (err) => {
         console.error(`Erro ao excluir o produto com ID ${id}:`, err);
         alert('Ocorreu um erro ao tentar excluir o produto.');
-      }
+      },
     });
   }
-  
+  filtrar(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const valor = input.value.toLowerCase();
+
+    if (!valor) {
+      this.produtosFiltrados = this.produtos;
+      return;
+    }
+
+    this.produtosFiltrados = this.produtos.filter((produto) => {
+      return (
+        produto.nome.toLowerCase().includes(valor) ||
+        produto.categoria.toLowerCase().includes(valor)
+      );
+    });
+  }
 }
